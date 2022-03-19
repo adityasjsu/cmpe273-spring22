31d3600dcd9ec0a05f17cdcf8585e801a15020d6
adityasjsu <aditya.kumar@sjsu.edu>
2022-03-18 23:33:05 -0700

    further item changes

---
+++ frontend/src/components/Item/Item.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import {useEffect, useState} from 'react';
+import { CCard, CCardImage, CCardBody, CCardTitle, CCardText, CButton } from '@coreui/react'
+import ItemEditPopup from './ItemEditPopup';
+import axios from 'axios';
+import { useNavigate } from 'react-router';
+
+
+function Item(props) {
+
+    const[seen, setSeen] = useState(false);
+    const[fav,setFav]= useState(false);
+    const navigate = useNavigate();
+    const[unFav, setUnfav] = useState("");
+ 
+    useEffect( () => {
+        //props.item.fav = props.item.fav;
+
+        if(props.item.fav === "1"){
+            setFav(true);
+            //setUnfav(false);
+        }
+        else{
+            setFav(false);
+            //setFav(false);
+        }
+    })
+
+    const handleFavorite = () => {
+
+        const data = {
+            fav:"1"
+        }
+        ;
+        axios.put("/api/items/fav/"+ props.item.product_ID, data)
+        .then(response => {
+            setFav(true);
+        })
+        console.log("Item Favorited")
+        window.location.reload(false);
+    }
+
+    const handleUnFav = () => {
+
+        const data = {
+            fav:"0"
+        }
+
+        axios.put("http://3.22.185.203:3001/api/items/fav/"+ props.item.product_ID, data)
+        .then(response => {
+            setFav(false);
+        })
+        console.log("Item UnFavorited")
+        window.location.reload(false);
+    }
+
+    const onNavigateItemPage = () => {
+        navigate("/item/" + props.item.product_ID);
+    }
+
+    const handleItemPopup = () => {
+        setSeen(!seen);
+        //console.log("Pressed:" , seen);
+    }
+
+    const handleDeleteItem = () => {
+        axios.delete("/api/items/"+ props.item.product_ID);
+        window.location.reload(false);
+
+    }
+
+    return (
+        <div className='hover' >
+        
+            {seen ? <ItemEditPopup item={props.item} toggle={handleItemPopup} /> : null}
+            {/* <button onClick={onNavigateItemPage}> */}         
+            <CCard  className={`mb-3 border-light border-top-3 border-top-light`} >
+            <CCardImage onClick={onNavigateItemPage} orientation="top" src={props.item.image} width={150} height={200}/>
+                <CCardBody>
+                <CCardTitle>{props.item.name}</CCardTitle>
+
+                <br/>
+                <CCardText>Price:{localStorage.getItem("currency")}{props.item.price}</CCardText>
+                <CCardText>Quantity:{props.item.quantity > 0 ? props.item.quantity : "Out of Stock"}</CCardText>          
+                
+                {/* <CButton color='light' onClick={onNavigateItemPage} href="/item">Item Page</CButton> */}
+                <br/>
+                
+                { fav ?  
+                
+                    <CButton color='danger' variant='outline' onClick={handleUnFav} >Add to favorite</CButton> 
+                
+                    :   
+                    
+                    <button color='success' variant='outline' onClick={handleFavorite} >Add to Favorite</button>
+                    
+                }
+                {/* <CButton onClick={handleFavorite} href="#">Favorite Item</CButton> */}
+
+                <br/> 
+                <br/>
+
+                {props.type === "shop" ?  
+                
+                <><button color='warning' shape='rounded-pill' onClick={handleItemPopup}>Edit</button></>  
+
+                :  ""}
+                
+            </CCardBody>
+            </CCard>
+            
+            {/* </button> */}
+            <br/>
+             
+
+        </div>     
+    
+    );
+}
+
+// Item.propTypes = {
+//   product: React.PropTypes.object.isRequired
+// };
+
+
+export default Item;
---
+++ frontend/src/components/Item/ItemPage.js
@@ -0,0 +1,194 @@
+import React , {useEffect, useState} from 'react';
+import axios from 'axios';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
+import { useParams } from 'react-router';
+import { CContainer, CRow, CCol, CButton } from '@coreui/react';
+
+const ItemPage = (props) => {
+
+    const[item, setItem]= useState({});
+    const[fav,setFav] = useState(false);
+    const[shopName, setShopName] = useState("");
+    const[sales, setSales]= useState(0);
+    const {id} = useParams();
+    const navigate = useNavigate();
+    let[counter, setCounter] = useState(1);
+
+
+    useEffect(() => {
+
+        async function getItem(){
+ 
+            let response = axios.get("/api/items/"+ id);
+            response = await response;
+
+            setItem(response.data[0]);
+            console.log(response.data[0])
+        }
+
+        getItem();
+
+    },[setItem]);
+
+
+    useEffect(()=> {
+
+        axios.get("/api/shops/"+ item.shopname)
+            .then(response => {
+                setShopName(response.data[0].name);
+                setSales(response.data[0].total_sales)
+                console.log(response.data[0].name)
+            });
+
+        if(item.fav === "1"){
+            setFav(true);
+        }
+        else{
+            setFav(false);
+        }
+
+    })
+
+
+    const handleFavorite = () => {
+
+        const data = {
+            fav:"1"
+        }
+        ;
+        axios.put("/api/items/fav/"+ item.product_ID, data)
+        .then(response => {
+            setFav(true);
+        })
+        console.log("Item Favorited")
+        window.location.reload(false);
+    }
+
+    const handleUnFav = () => {
+
+        const data = {
+            fav:"0"
+        }
+
+        axios.put("/api/items/fav/"+ item.product_ID, data)
+        .then(response => {
+            setFav(false);
+        })
+        console.log("Item UnFavorited")
+        window.location.reload(false);
+    }
+
+    const onNavigateShopPage = () => {
+        navigate("/shop/" + item.shopname);
+    }
+
+    const handlePlus = () => {
+        if(counter <= item.quantity)
+        setCounter(counter++)
+    }
+
+    const handleMinus = () => {
+        if(counter > 0)
+        setCounter(counter--)
+    }
+
+    const handleAddToCart = (e) => {
+
+
+        const cartData = {
+            cart_item_ID:id,
+            image:item.image,
+            name:item.name,
+            shop:item.shopname,
+            quantity:counter,
+            stock:item.quantity,
+            price:item.price*counter,
+            email:sessionStorage.getItem("token")
+        }
+
+        axios.post("/api/cart/" , cartData)
+        .then(response => {
+            console.log(response);
+            console.log("Item Added to cart");
+            console.log(cartData);
+        })
+
+
+        // handleQuantityChange()
+        navigate("/cart/");
+
+    }
+
+  
+
+    return(
+        <><div>
+            {/* {seen ? <ShopPopup name="TESTING" toggle={handlePopup} /> : null} */}
+            <br /><br /><br /><br /><br />
+            <CContainer>
+
+            <CRow className="justify-content-between">
+                <CCol ><img src={item.image} width={600}/></CCol>
+
+                <CCol xs={2}>
+
+
+                    
+
+                </CCol>
+
+                <CCol>
+                    
+                    <h3><b>{item.name}</b></h3>
+
+                    <br />
+                    Seller:<button color='link' onClick={onNavigateShopPage}>{shopName}</button> Sales: {sales}
+                   
+                    
+                    <br /><br />
+                    Price <b>{localStorage.getItem("currency") + item.price}</b>
+
+                    <br />
+                    Description: <b>{item.description}</b>
+
+                    <br /><br />
+                    Stock: <b>{item.quantity > 0 ? item.quantity : "Out of Stock"}</b>
+
+                    <br /><br />
+                    {item.quantity > 0      ?
+
+                        <><button variant='outline' onClick={handleMinus}>-</button><b>   {counter}   </b><button variant='outline' onClick={handlePlus}>+</button></>
+
+                    : ""}
+
+                    <br/>
+                    <br/>
+                    <br/>
+
+                    {item.quantity > 0      ?
+                    
+                        <button onClick={handleAddToCart} color='' variant='outline' >Add to Cart: {localStorage.getItem("currency")}{item.price*counter}</button>
+
+                    :
+                    
+                    ""}
+{ fav ?  
+                    
+                    <button color='link' onClick={handleUnFav} >Add to Favorites</button> 
+                
+                    :   
+                    
+                    <button color='link' onClick={handleFavorite} >Add to Favorites</button>
+                
+                    }                    
+                </CCol>
+            </CRow>
+            </CContainer>
+
+        </div>
+       </>
+    )
+}
+
+export default ItemPage;
